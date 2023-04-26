da3a8dea38777b0cc75a106499a0e48d33f97d05
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-04-27 00:06:16 +0300

    backend:  added update and delet to kitchen controller

--- FoodAlley-server/food-alley/app/Http/Controllers/KitchenController.php
+++ FoodAlley-server/food-alley/app/Http/Controllers/KitchenController.php
@@ -22,6 +22,25 @@ class KitchenController extends Controller
         $kitchen = Kitchen::create($validatedData);
         return response()->json($kitchen);
     }
+    public function show(Kitchen $kitchen)
+    {
+        return response()->json($kitchen);
+    }
 
+    public function update(Request $request, Kitchen $kitchen)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255|unique:kitchens,name,'.$kitchen->id,
+        ]);
+
+        $kitchen->update($validatedData);
+        return response()->json($kitchen);
+    }
+
+    public function destroy(Kitchen $kitchen)
+    {
+        $kitchen->delete();
+        return response()->json(['message' => 'Kitchen deleted successfully']);
+    }
     
 }
