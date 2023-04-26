a9e410dc36be1ccfbc8bab7a099abf96572c9696
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-04-27 00:08:51 +0300

    backend:  created kitchen model

--- FoodAlley-server/food-alley/app/Models/Kitchen.php
+++ FoodAlley-server/food-alley/app/Models/Kitchen.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Kitchen extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function recipes()
+    {
+        return $this->hasMany(Recipe::class);
+    }
 }
