c40ddb4a3a2ac6abfd396ee80e0e63c3203433eb
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-04-27 00:56:29 +0300

    backend: created recipe-ingrident controller

---
+++ FoodAlley-server/food-alley/app/Http/Controllers/recipeIngredient.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RecipeIngredient;
+use Illuminate\Http\Request;
+
+
+class RecipeIngredientController extends Controller
+{
+    public function index()
+    {
+        $recipeIngredients = RecipeIngredient::all();
+        return response()->json($recipeIngredients);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'recipes_id' => 'required|exists:recipes,id',
+            'ingredients_id' => 'required|exists:ingredients,id',
+            'quantity' => 'required|string|max:255',
+        ]);
+
+        $recipeIngredient = RecipeIngredient::create($validatedData);
+        return response()->json($recipeIngredient, 201);
+    }
+
+    public function show($id)
+    {
+        $recipeIngredient = RecipeIngredient::findOrFail($id);
+        return response()->json($recipeIngredient);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $recipeIngredient = RecipeIngredient::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'recipes_id' => 'sometimes|required|exists:recipes,id',
+            'ingredients_id' => 'sometimes|required|exists:ingredients,id',
+            'quantity' => 'sometimes|required|string|max:255',
+        ]);
+
+        $recipeIngredient->update($validatedData);
+        return response()->json($recipeIngredient);
+    }
+
+    public function destroy($id)
+    {
+        $recipeIngredient = RecipeIngredient::findOrFail($id);
+        $recipeIngredient->delete();
+        return response()->json(null, 204);
+    }
+}
