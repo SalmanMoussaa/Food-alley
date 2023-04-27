baed19037da8d842f25468f31412f968ae08bbad
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-04-27 10:21:13 +0300

    backend: created restricted food controller

---
+++ FoodAlley-server/food-alley/app/Http/Controllers/RestrictedFoodController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Model\restrictedFoodController;
+
+use Illuminate\Http\Request;
+
+class RestrictedFoodController extends Controller
+{
+    public function index()
+    {
+        $restrictedFoods = RestrictedFood::all();
+    
+        return response()->json($restrictedFoods);
+    }
+    
+    public function show($id)
+    {
+        $restrictedFood = RestrictedFood::findOrFail($id);
+    
+        return response()->json($restrictedFood);
+    }
+    
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'allergie_id' => 'required|exists:allergies,id',
+            'ingredient_id' => 'required|exists:ingredients,id'
+        ]);
+    
+        $restrictedFood = RestrictedFood::create($validatedData);
+    
+        return response()->json($restrictedFood, 201);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $restrictedFood = RestrictedFood::findOrFail($id);
+    
+        $validatedData = $request->validate([
+            'allergie_id' => 'required|exists:allergies,id',
+            'ingredient_id' => 'required|exists:ingredients,id'
+        ]);
+    
+        $restrictedFood->update($validatedData);
+    
+        return response()->json($restrictedFood, 200);
+    }
+    
+    public function destroy($id)
+    {
+        $restrictedFood = RestrictedFood::findOrFail($id);
+    
+        $restrictedFood->delete();
+    
+        return response()->json(null, 204);
+    }
+}
+?>
