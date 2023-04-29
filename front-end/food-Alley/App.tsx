d799beef0529f2415a73fef845cb44790098fa6c
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-04-30 00:19:16 +0300

    front-end: editedt button componnent

--- front-end/food-Alley/App.tsx
+++ front-end/food-Alley/App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native';
+import Buttons from "./src/screens/components/Button";
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {
-      <Text>s</Text>
+      <Buttons title="Press me" onPress={() =>console.log("salman  vcpressed")} />
       } 
     </SafeAreaView>
   );
--- front-end/food-Alley/src/screens/components/button.tsx
+++ front-end/food-Alley/src/screens/components/button.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   height: 46,
   left: 119,
   top: 597,
-borderradius: 8
+  borderRadius: 8
   },
   buttonText: {
     color: 'white',
