70b1438e923b5134b4b4224ec31e8cbd0e2ae0da
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-04-29 23:50:23 +0300

    front-end: created button componnent

---
+++ front-end/food-Alley/src/screens/components/button.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+interface Props {
+  title: string;
+  onPress: () => void;
+  disabled?: boolean;
+}
+
+const Button: React.FC<Props> = ({ title, onPress, disabled = false }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress} disabled={disabled}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#333333',
+    position: 'absolute',
+  width: 146,
+  height: 46,
+  left: 119,
+  top: 597,
+borderradius: 8
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default Button;
