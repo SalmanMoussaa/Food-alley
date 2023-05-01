d846e1c2c7d54d2720f24577006e61dec6feeadb
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-02 02:54:02 +0300

    front-end: added food api to get ingridients

--- FoodAlley-server/food-alley/app/Http/Controllers/IngredientController.php
+++ FoodAlley-server/food-alley/app/Http/Controllers/IngredientController.php
@@ -20,10 +20,42 @@ class IngredientController extends Controller
             'category' => 'required|string|max:255',
             'img_url' => 'required|string|max:255'
         ]);
-
-        $ingredient = Ingredient::create($validated);
-        return response()->json(['data' => $ingredient]);
+    
+        // Use Spoonacular API to generate a random ingredient name
+        $curl = curl_init();
+    
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.spoonacular.com/food/ingredients/random?apiKey=230ac152a7f14e4193ac6b436baf010e',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ));
+    
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+    
+        curl_close($curl);
+    
+        if ($err) {
+            return response()->json(['error' => "cURL Error #:" . $err], 500);
+        }
+    
+        $ingredient = json_decode($response, true);
+    
+        // Create the new ingredient in the database
+        $newIngredient = Ingredient::create([
+            'name' => $ingredient['name'],
+            'category' => $validated['category'],
+            'img_url' => $validated['img_url']
+        ]);
+    
+        return response()->json(['data' => $newIngredient]);
     }
+    
 
     public function destroy($id)
     {
