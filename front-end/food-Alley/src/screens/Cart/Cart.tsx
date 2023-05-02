9fc6de546e01f382538a17fc552651245d9556d3
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-02 19:57:15 +0300

    front-end: updated product cart imports to import DrpoDown picker

--- front-end/food-Alley/src/screens/Cart/Cart.tsx
+++ front-end/food-Alley/src/screens/Cart/Cart.tsx
@@ -1,6 +1,8 @@
-import * as React from "react";
-import { Text, StyleSheet, Pressable, View, Image } from "react-native";
-import { FontFamily, FontSize, Color, Border } from "../components/GlobalStyles";
+import React, { useState } from "react";
+import { Image, StyleSheet, Pressable, View, Text } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { useNavigation } from "@react-navigation/native";
+import { FontFamily, Color, FontSize, Border } from "../GlobalStyles";
 import ProdcutinCart from "../components/Productcart";
 
 const Cart = () => {
