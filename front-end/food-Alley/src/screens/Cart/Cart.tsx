1bea863d9ba17ba3d7a2453f0b01b6f1fa327f46
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-02 06:55:18 +0300

    front-end: created the  father view componnet

--- front-end/food-Alley/src/screens/Cart/Cart.tsx
+++ front-end/food-Alley/src/screens/Cart/Cart.tsx
@@ -1,3 +1,9 @@
 import * as React from "react";
 import { Text, StyleSheet, Pressable, View, Image } from "react-native";
 import { FontFamily, FontSize, Color, Border } from "../components/GlobalStyles";
+const Cart = () => {
+    return(
+
+        <View></View>
+    );
+};
