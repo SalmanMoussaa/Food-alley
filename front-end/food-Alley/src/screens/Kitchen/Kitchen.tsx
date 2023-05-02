abb8f33b933910405c2071a341b1f160e48eb89c
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-02 06:03:28 +0300

    front-end: added food types and food items to resturant page

--- front-end/food-Alley/src/screens/Kitchen/Kitchen.tsx
+++ front-end/food-Alley/src/screens/Kitchen/Kitchen.tsx
@@ -7,14 +7,50 @@ import Discount from "../components/Discount";
 import Bar from "../components/bar";
 import Kitchenscomponent from "../components/Kitchenscomponent";
 import Kitchenpagecomp from "../components/kitchenpagecomp";
+import Foodtype from "../components/Foodtype";
+
 
 const Kitchen = () => {
     return (
       <View>
         
         <Kitchenpagecomp/>
+        <View style={[styles.search]}>
+        <Searchbarcomp />
+        </View>
+        <View style={[styles.parenttypes]}>
+            <Foodtype/>
+
+        </View>
+        <View style={[styles.parentfoodItems]}><FoodItem foodname={"salman"} imageUri={""} kitchenName={"beirut"}/>
+        
+        </View>
+        
       </View>
     );
   };
+  const styles =StyleSheet.create({
+    search:{
+        top: "60%",
+    width: "120%",
+    left: -45,
+    backgroundColor:"#FFFFFF",
+
+
+    },
+    parentfoodItems:{
+        left:"6%",
+        top:"110%"
+    },
+    parenttypes:{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"space-between",
+        left:"3%",
+        top:"70%"
+    }
+
+
+  })
   
   export default Kitchen;
