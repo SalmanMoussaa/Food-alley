9474b1caf452a40dea43201d22075b143465b2fe
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-02 22:34:49 +0300

    front-end:designed the page Structure

--- front-end/food-Alley/src/screens/ProductPage/ProductPage.tsx
+++ front-end/food-Alley/src/screens/ProductPage/ProductPage.tsx
@@ -28,6 +28,92 @@ const ProductPage = () => {
         horizontal
         contentContainerStyle={styles.productPageScrollViewContent}
       >
+        <View style={styles.vectorParent}>
+          <Image
+            style={[styles.frameChild, styles.frameChildPosition]}
+            resizeMode="cover"
+            source={require("../assets/rectangle-32.png")}
+          />
+          <Text style={[styles.filterByAllergy, styles.americanFlexBox]}>
+            filter by allergy
+          </Text>
+          <Text style={[styles.americanCheeseBurger, styles.americanFlexBox]}>
+            American cheese burger
+          </Text>
+          <Text style={[styles.text, styles.americanFlexBox]}>60$</Text>
+          <ImageBackground
+            style={[styles.arrowLeft5Wrapper, styles.frameChildPosition]}
+            resizeMode="cover"
+            source={require("../assets/frame26.png")}
+          >
+            <Pressable
+              style={styles.arrowLeft5}
+              onPress={() => navigation.goBack()}
+            >
+              <Image
+                style={styles.icon}
+                resizeMode="cover"
+                source={require("../assets/arrowleft-5.png")}
+              />
+            </Pressable>
+          </ImageBackground>
+          <View
+            style={[
+              styles.theAmericanCheeseburgerIsAWrapper,
+              styles.vectorGroupLayout,
+            ]}
+          >
+            <Text
+              style={[styles.theAmericanCheeseburger, styles.americanFlexBox]}
+            >
+              The American cheeseburger is a classic fast-food staple, featuring a
+              juicy beef patty, melted American cheese, and a variety of toppings
+              on a toasted bun, making it a beloved and iconic American dish.
+            </Text>
+          </View>
+          <View style={[styles.vectorGroup, styles.vectorGroupLayout]}>
+            <Image
+              style={[styles.frameItem, styles.rectangleLayout]}
+              resizeMode="cover"
+              source={require("../assets/rectangle-35.png")}
+            />
+            <RNKCheckBox
+              style={styles.frameInner}
+              checked={frameCheckboxchecked}
+              onChange={() => setFrameCheckboxchecked(!frameCheckboxchecked)}
+            />
+          </View>
+          <Text style={[styles.removeIngredient, styles.americanFlexBox]}>
+            remove ingredient
+          </Text>
+          <View style={[styles.wrapper, styles.rectangleLayout]}>
+            <DropDownPicker
+              style={styles.dropdownpicker}
+              open={frameDropdownOpen}
+              setOpen={setFrameDropdownOpen}
+              value={frameDropdownValue}
+              setValue={setFrameDropdownValue}
+              placeholder="allergy  name  "
+              labelStyle={styles.frameDropdownValue}
+              dropDownContainerStyle={styles.frameDropdowndropDownContainer}
+            />
+          </View>
+          <RNKTextInput
+            style={[styles.rectangleRnktextinput, styles.rectangleLayout]}
+            placeholder="Place your text"
+            value={rectangleTextInput}
+            onChangeText={setRectangleTextInput}
+          />
+          <Text style={[styles.specialInstructions, styles.americanFlexBox]}>
+            special instructions
+          </Text>
+          <Pressable
+            style={[styles.rectanglePressable, styles.rectangleLayout]}
+          />
+          <Text style={[styles.addToCart, styles.addToCartTypo]}>
+            Add to Cart
+          </Text>
+        </View>
         
       </ScrollView>
     );
