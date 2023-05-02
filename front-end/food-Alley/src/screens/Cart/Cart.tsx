d86eaa6fe67e601421706115bfe6d79ed4fffbbf
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-02 20:02:25 +0300

    front-end: added cart page Styling

--- front-end/food-Alley/src/screens/Cart/Cart.tsx
+++ front-end/food-Alley/src/screens/Cart/Cart.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Image, StyleSheet, Pressable, View, Text } from "react-native";
 import DropDownPicker from "react-native-dropdown-picker";
 import { useNavigation } from "@react-navigation/native";
-import { FontFamily, Color, FontSize, Border } from "../GlobalStyles";
+import { FontFamily, Color, FontSize, Border } from "../components/GlobalStyles";
 import ProdcutinCart from "../components/Productcart";
 
 const Cart = () => {
@@ -70,4 +70,151 @@ const navigation = useNavigation();
     </View>
     );
 };
-export default Cart;
+const styles = StyleSheet.create({
+    frameDropdownValue: {
+      color: "#e2e7f0",
+      fontSize: 20,
+      fontWeight: "800",
+      fontFamily: "Inter_extrabold",
+    },
+    frameDropdownText: {
+      color: "#fe5932",
+      fontSize: 14,
+      fontWeight: "800",
+      fontFamily: "Inter_extrabold",
+    },
+    wrapperPosition: {
+      width: 393,
+      left: 0,
+      position: "absolute",
+    },
+    groupChildLayout: {
+      height: 55,
+      width: 216,
+      position: "absolute",
+    },
+    placeOrderTypo: {
+      textAlign: "left",
+      fontFamily: FontFamily.interExtrabold,
+      fontWeight: "800",
+      position: "absolute",
+    },
+    taxTypo: {
+      color: Color.white,
+      textAlign: "left",
+      fontFamily: FontFamily.interExtrabold,
+      fontWeight: "800",
+      fontSize: FontSize.size_mini,
+      position: "absolute",
+    },
+    text1Typo: {
+      top: 618,
+      color: Color.white,
+      textAlign: "left",
+      fontFamily: FontFamily.interExtrabold,
+      fontWeight: "800",
+      fontSize: FontSize.size_mini,
+      position: "absolute",
+    },
+    textTypo: {
+      left: 335,
+      color: Color.white,
+      textAlign: "left",
+      fontFamily: FontFamily.interExtrabold,
+      fontWeight: "800",
+      fontSize: FontSize.size_mini,
+      position: "absolute",
+    },
+    yourFoodCart: {
+      top: 57,
+      left: 138,
+      width: 142,
+      height: 20,
+      position: "absolute",
+    },
+    cartChild: {
+      top: 557,
+      height: 295,
+    },
+    line26Stroke: {
+      top: 635,
+      height: 1,
+    },
+    groupChild: {
+      top: 0,
+      borderRadius: Border.br_3xs,
+      backgroundColor: Color.tomato,
+      left: 0,
+      width: 216,
+    },
+    placeOrder: {
+      top: 19,
+      left: 61,
+      color: Color.wFBase300,
+      fontSize: FontSize.size_mini,
+      textAlign: "left",
+      fontFamily: FontFamily.interExtrabold,
+      fontWeight: "800",
+    },
+    rectangleParent: {
+      top: 743,
+      left: 84,
+    },
+    wrapper: {
+      top: 433,
+      height: 94,
+    },
+    chooseLocation: {
+      top: 404,
+      left: 20,
+      fontSize: FontSize.size_lg,
+      color: Color.black,
+    },
+    icon: {
+      height: "100%",
+      width: "100%",
+    },
+    arrowLeft3: {
+      left: 18,
+      top: 37,
+      width: 50,
+      height: 50,
+      position: "absolute",
+    },
+    subTotal: {
+      left: 25,
+      top: 664,
+    },
+    cartTotal: {
+      left: 17,
+    },
+    delivery: {
+      left: 19,
+      top: 582,
+    },
+    tax: {
+      left: 24,
+      top: 600,
+    },
+    text: {
+      top: 664,
+    },
+    text1: {
+      left: 336,
+    },
+    text2: {
+      top: 600,
+    },
+    text3: {
+      top: 582,
+    },
+    cart: {
+      backgroundColor: Color.wFBase200,
+      flex: 1,
+      height: 852,
+      overflow: "hidden",
+      width: "100%",
+    },
+  });
+  
+  export default Cart;
