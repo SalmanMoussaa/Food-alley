3a859fcdca6fb3bed917d95ff83cd3f6860c4384
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-02 07:06:20 +0300

    front-end: added base look to the componnet

--- front-end/food-Alley/src/screens/components/Productcart.tsx
+++ front-end/food-Alley/src/screens/components/Productcart.tsx
@@ -1,3 +1,38 @@
 import * as React from "react";
 import { Image, StyleSheet, View } from "react-native";
 import { Border } from "./GlobalStyles";
+const ProdcutinCart = () => {
+    return (
+      <View style={styles.prodcutincart}>
+        <Image
+          style={styles.prodcutincartChild}
+          resizeMode="cover"
+          source={require("../assets/rectangle-49.png")}
+        />
+        <Text  style={styles.priceIcon}>
+           
+            20$
+            </Text>
+          
+        <Image
+          
+          resizeMode="cover"
+          source={require("../assets/product-name.png")}
+        />
+        <Text style={styles.productNameIcon}>
+        
+            20$
+            </Text>
+        <Image
+          style={styles.xIcon}
+          resizeMode="cover"
+          source={require("../assets/x.png")}
+        />
+        <Image
+          style={styles.prodcutincartItem}
+          resizeMode="cover"
+          source={require("../assets/rectangle-18.png")}
+        />
+      </View>
+    );
+  };
