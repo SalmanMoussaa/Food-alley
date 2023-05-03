786daaaa2e4bedda742998778b99f734453dafe5
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-04 01:06:18 +0300

    front-end: aded basic linking to the serach bar componnet

--- front-end/food-Alley/src/screens/components/Searchbarcomp.tsx
+++ front-end/food-Alley/src/screens/components/Searchbarcomp.tsx
@@ -3,46 +3,60 @@ import { Searchbar } from 'react-native-paper';
 import { View, Image, StyleSheet, Pressable, Text, Dimensions, SafeAreaView, ImageSourcePropType } from "react-native";
 import { transparent } from 'react-native-paper/lib/typescript/src/styles/themes/v2/colors';
 
-
 const Searchbarcomp = () => {
   const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchResults, setSearchResults] = React.useState([]);
 
-  const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
+  const fetchRecipes = async (query: any) => {
+    try {
+      const response = await fetch(`https://your-api-endpoint.com/recipes?name=${query}`);
+      const results = await response.json();
+      setSearchResults(results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const onChangeSearch = (query: React.SetStateAction<string>) => {
+    setSearchQuery(query);
+    fetchRecipes(query);
+  };
 
   return (
-    <Searchbar
-    
-    style={styles.search}
-    textAlign={'left'}
-    textAlignVertical="center"
-    
-    
-
-      placeholder="Search"
-      onChangeText={onChangeSearch}
-      value={searchQuery}
-    />
+    <View>
+      <Searchbar
+        style={styles.search}
+        textAlign={'left'}
+        textAlignVertical="center"
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+      <View style={styles.searchResults}>
+        {searchResults.map((recipe) => (
+          <Text key={recipe.id}>{recipe.name}</Text>
+        ))}
+      </View>
+    </View>
   );
 };
+
 const styles = StyleSheet.create({
-search:{
-  display:"flex",
-  backgroundColor:"#FFFFFF",
-  borderColor:"ef4f",
-  borderWidth:2,
-  width:"80%",
-  alignSelf:"center",
-  
-  borderRadius:10,
-  textAlign:"justify",
-  paddingTop:0
-  
-  
-  
-  
-},
-
+  search: {
+    backgroundColor: "#FFFFFF",
+    borderColor: "ef4f",
+    borderWidth: 2,
+    width: "80%",
+    alignSelf: "center",
+    borderRadius: 10,
+    textAlign: "justify",
+    paddingTop: 0
+  },
+  searchResults: {
+    marginTop: 10,
+    marginLeft: 20,
+    marginRight: 20
+  }
 });
-
 
 export default Searchbarcomp;
