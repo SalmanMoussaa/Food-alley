e5200fcb50cc01e90d290ece88cbaeda043a4458
SalmanMoussaa <salman.m.moussaa@gmail.com>
2023-05-15 17:48:02 +0300

    front-end: search bar comp functionalty

--- front-end/food-Alley/src/screens/components/Searchbarcomp.tsx
+++ front-end/food-Alley/src/screens/components/Searchbarcomp.tsx
@@ -3,6 +3,7 @@ import { Searchbar } from 'react-native-paper';
 import { View, StyleSheet, FlatList, ListRenderItemInfo, Text } from 'react-native';
 import axios from 'axios';
 import FoodItem from './FoodItem';
+import { Color } from './GlobalStyles';
 
 interface Recipe {
   id: string;
@@ -19,14 +20,17 @@ const Searchbarcomp = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState<Recipe[]>([]);
   const [showResults, setShowResults] = useState(false);
+  const[refresh,setrefresh]= useState(false);
 
   const fetchRecipes = async (query: string) => {
+    setrefresh(true);
     try {
       const response = await axios.get(`http://10.0.2.2:8000/api/recipes/search?query=${query}`);
       const results = response.data;
       console.log(response.data);
       setSearchResults(results);
       setShowResults(true);
+      setrefresh(false);
     } catch (error) {
       console.error(error);
     }
@@ -55,15 +59,22 @@ const Searchbarcomp = () => {
         onFocus={() => setShowResults(true)}
         onBlur={() => setShowResults(false)}
       />
-      {showResults && searchResults.length === 0 ? (
+        <View style={styles.flatlist}>
+      {!showResults || searchResults.length === 0 ? (
         <Text>No results found.</Text>
-      ) : showResults && (
+      ) : (
         <FlatList
-          data={searchResults}
-          renderItem={renderRecipeItem}
-          keyExtractor={(item) => item.id}
+        
+        extraData={searchResults}
+        data={searchResults}
+        renderItem={renderRecipeItem}
+        keyExtractor={(item) => item.id}
         />
-      )}
+        )
+        
+      }
+      </View>
+        
     </View>
   );
 };
@@ -81,11 +92,31 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     position:"absolute"
   },
+
+  flatlist:{
+    /* flex:1,
+
+    display:"flex", */
+    height:200,
+    width:200,
+    position:"absolute",
+    top:0,
+    bottom:0,
+    left:0,
+
+  }
+  ,
   searchResults: {
     marginTop: 10,
     marginLeft: 20,
     marginRight: 20,
   },
+  container:{
+   width:500,
+   height:500
+  
+
+  }
 });
 
 export default Searchbarcomp;
